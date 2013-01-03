22ded94b2b110b5068edba77fe0f32e7a9d42b1c
Kevin Peterson <kevin.peterson@mayo.edu>
2013-01-03 10:12:02 -0600

    Added batch processing.

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 '''
-Simple UMLS CUI to SNOMEDCT CTS2 Mapping Service.
+A Simple UMLS CUI to SNOMEDCT CTS2 MapEntry Service.
 '''
 __author__ = "Kevin Peterson"
 __credits__ = ["Harold Solbrig"]
@@ -11,7 +11,9 @@ __email__ = "kevin.peterson@mayo.edu"
 from flask import Flask
 from flask import jsonify
 from flask import Response
+from flask import request
 from collections import defaultdict
+from collections import OrderedDict
 import string
 import datetime
 import argparse
@@ -56,6 +58,32 @@ mapTargetXml = """
 		</mapTarget>
 """
 
+mapTargetListListXml = """<?xml version="1.0" encoding="UTF-8"?>
+<MapTargetListList 
+    xmlns="http://schema.omg.org/spec/CTS2/1.0/MapEntryServices"
+    xmlns:core="http://schema.omg.org/spec/CTS2/1.0/Core"
+    xmlns:mapVersion="http://schema.omg.org/spec/CTS2/1.0/MapVersion"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://schema.omg.org/spec/CTS2/1.0/MapEntryServices http://www.omg.org/spec/cts2/201206/mapversion/MapEntryServices.xsd">
+    {targets}
+</MapTargetListList>
+"""
+
+mapTargetListListEntryXml = """
+    <entry>
+        {targets}
+    </entry>
+"""
+
+mapTargetListListEntryEntryXml = """
+    <entry entryOrder="{entryNum}">
+        <mapVersion:mapTo uri="http://snomed.info/id/{code}" href="http://informatics.mayo.edu/cts2/services/sct/cts2/codesystem/SNOMED_CT_core/version/20120731/entity/{code}">
+            <core:namespace>sctid</core:namespace>
+            <core:name>{code}</core:name>
+        </mapVersion:mapTo>
+    </entry>
+"""
+
 mapEntryNotFoundXml = """<?xml version="1.0" encoding="UTF-8"?>
 <UnknownResourceReference
     xmlns="http://schema.omg.org/spec/CTS2/1.0/Exceptions"
@@ -69,15 +97,44 @@ mapEntryNotFoundXml = """<?xml version="1.0" encoding="UTF-8"?>
 </UnknownResourceReference>
 """
 
-def get_map_entry_xml(cui, codes):
+class EntryNotFoundError(Exception):
+    def __init__(self, code):
+        self.code = code
+    def __str__(self):
+        return repr(self.code)
+
+@app.errorhandler(404)
+def not_found(error):
+    xml = mapEntryNotFoundXml.format(cui=error.code)
+    return Response(xml, mimetype='text/xml'), 404
+
+def get_targetlistlist_xml(cuis):
+    '''Build the CTS2 MapTargetListList XML'''
+    entries = ''
+    entryNum = 1;
+    for cui in cuis:
+        if not cui in cuiToCodes:
+            raise EntryNotFoundError(cui)
+        targets = ""
+        for code in cuiToCodes[cui]:
+            targets += mapTargetListListEntryEntryXml.format(code=code,entryNum=entryNum)
+            entryNum += 1
+        entries += mapTargetListListEntryXml.format(targets=targets)
+
+    return mapTargetListListXml.format(cui=cui, targets=entries, date=datetime.datetime.now().isoformat())
+
+def get_map_entry_xml(cui):
     '''Build the CTS2 MapEntry XML'''
+    if not cui in cuiToCodes:
+        raise EntryNotFoundError(cui)
+
     targets = ''
     entryNum = 1;
-    for code in codes:
-    	targets = targets + mapTargetXml.format(code=code,entryNum=entryNum)
+    for code in cuiToCodes[cui]:
+    	targets += mapTargetXml.format(code=code,entryNum=entryNum)
     	entryNum += 1
 
-	return mapEntryXml.format(cui=cui, targets=targets, date=datetime.datetime.now().isoformat())
+    return mapEntryXml.format(cui=cui, targets=targets, date=datetime.datetime.now().isoformat())
 
 def load_file():
     '''Load the data file'''
@@ -92,14 +149,28 @@ def load_file():
         cuiToCodes[cui].add(code)
     file.close()
 
+@app.route("/map/UMLS_TO_SNOMEDCT/version/1.0/resolution")
+def get_map_targetlistlist():
+    '''Get a MapTargetListList based on a list of UMLS CUIs'''
+    cuis = remove_duplicates(request.args['mapfrom'].split(','))
+    try:
+        return Response(get_targetlistlist_xml(cuis), mimetype='text/xml') 
+    except EntryNotFoundError as e:
+        return not_found(e)
+
+def remove_duplicates(lst):
+    dset = set()
+    # relies on the fact that dset.add() always returns None.
+    return [ l for l in lst if 
+             l not in dset and not dset.add(l) ] 
+
 @app.route("/map/UMLS_TO_SNOMEDCT/version/1.0/entry/<cui>")
 def get_map_entry(cui):
     '''Get a MapEntry based on a UMLS CUI'''
-    if cui in cuiToCodes:
-    	return Response(get_map_entry_xml(cui,cuiToCodes[cui]), mimetype='text/xml')
-    else:
-    	xml = mapEntryNotFoundXml.format(cui=cui)
-    	return Response(xml, mimetype='text/xml'), 404
+    try:
+    	return Response(get_map_entry_xml(cui), mimetype='text/xml')
+    except EntryNotFoundError as e:
+        return not_found(e)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -111,4 +182,4 @@ if __name__ == "__main__":
     load_file()
     print "Done loading."
 
-    app.run(debug=False,host='0.0.0.0',port=port)
+    app.run(debug=True,host='0.0.0.0',port=port)
